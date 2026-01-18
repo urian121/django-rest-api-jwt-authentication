3f6bd2cfe5a124dfde9677de1dc5bcf91aac8c5f
URIAN VIERA <urian1213viera@gmail.com>
2026-01-17 22:28:20 -0500

    permitiendo el acceso a la rute get para obtener todas las tareas sin necesidad de token

--- api/views.py
+++ api/views.py
@@ -1,4 +1,7 @@
-from rest_framework import viewsets # Importando viewsets de rest_framework
+from rest_framework import viewsets
+
+# Importando AllowAny y IsAuthenticated para permisos de acceso
+from rest_framework.permissions import AllowAny, IsAuthenticated
 from .models import Task # Importando Task de models
 from .serializers import TaskSerializer # Importando TaskSerializer de serializers
 
@@ -7,3 +10,9 @@ class TaskViewSet(viewsets.ModelViewSet):
     queryset = Task.objects.all()
     # Serializador para el modelo Task
     serializer_class = TaskSerializer
+    
+    # Permitir acceso público a la lista de tareas (AllowAny), sobreescribiendo el método get_permissions
+    def get_permissions(self):
+        if self.action == 'list':
+            return [AllowAny()]
+        return [IsAuthenticated()]
